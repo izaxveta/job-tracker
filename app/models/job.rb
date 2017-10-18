43f255986b26e68f969be12061318d4bdd7b7826
Iza Jimenez <izavetha@gmail.com>
2017-10-17 19:25:09 -0600

    Modified comment time formatting for view

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
-  belongs_to :company, dependent: :destroy
+  belongs_to :company
   belongs_to :category
   has_many :comments
 end
